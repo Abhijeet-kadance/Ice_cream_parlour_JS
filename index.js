6d4e5e90a1cf8dafddd8f0e6566e21faf0ef3a23
Abhijeet-Kadance <abhijeet.kadance.96@gmail.com>
2022-02-15 11:50:51 +0530

    Promise chaining

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const stocks = {
 // }
 
 const order = (time,work) => {
-    console.log("Test....")
+    // console.log("Test....")
     return new Promise((resolve,reject)=>{
         if(is_shop_open){
             setTimeout(()=>{
@@ -35,3 +35,32 @@ const order = (time,work) => {
 order(2000,
     ()=>console.log(`${stocks.Fruits[0]} was selected`)
 )
+
+.then(()=>{
+    return order(2000,()=>console.log("Production has Started"))
+})
+
+.then(()=>{
+    return order(3000,()=>console.log("Fruits have been chopped....."))
+})
+
+
+.then(()=>{
+    return order(1000,()=>  console.log(`Added ${stocks.liquid[0]} and ${stocks.liquid[1]} to the Machine.....`))
+})
+
+.then(()=>{
+    return order(1000,()=>console.log("Started the machine......."))
+})
+
+.then(()=>{
+    return order(2000,()=>console.log(`Seleceted ${stocks.holder[1]} as a container.....`))
+})
+
+.then(()=>{
+    return order(2000,()=> console.log(`Selected ${stocks.toppings[1]} as toppings.....`))
+})
+
+.then(()=>{
+    return order(1000,()=>{console.log("Your Ice-Cream is ready.. Here You Go.............")})
+})
